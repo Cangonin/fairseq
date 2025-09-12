8b9e31df3bf27766a4bdd82ea9551cf68b478404
Céline Angonin <89964843+Cangonin@users.noreply.github.com>
2025-09-12 15:52:57 +0200

    Run from hubert checkpoint, fix batch_size and change config param (#6)
    * make the code run from a checkpoint
    
    * Change config parameters and fix batch_size

--- fairseq/tasks/hubert_mtl_pretraining.py
+++ fairseq/tasks/hubert_mtl_pretraining.py
@@ -183,7 +183,7 @@ class HubertMTLPretrainingTask(HubertPretrainingTask):
             is_dataset_split_train = "train" in dataset.manifest_path
             batches = UnevenBatchSampler(
                 len(dataset),
-                batch_size=8,  # TODO: use max_tokens somehow instead or define the batch size in the config
+                batch_size=max_sentences,  # max_sentences corresponds effectively to the batch size
                 num_samples_unlabelled_dataset=dataset.ssl_num_samples,
                 repeat_supervised_samples=is_dataset_split_train,
             )
